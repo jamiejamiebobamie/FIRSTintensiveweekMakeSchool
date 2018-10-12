87f9691d79b2e799f3bed2b4612ec5bcec24a140
jamiejamiebobamie <jmccrory@vt.edu>
2018-10-12 10:56:24 -0700

    Users can create users

--- app.js
+++ app.js
@@ -3,20 +3,48 @@ const app = express()
 
 var exphbs = require('express-handlebars');
 
+// INITIALIZE BODY-PARSER AND ADD IT TO APP
+const bodyParser = require('body-parser');
+
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/rotten-potatoes');
+
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
-let users = [
-    {name: "Frank"},
-    {name: "Cynthia"}
-];
+// The following line must appear AFTER const app = express() and before your routes!
+app.use(bodyParser.urlencoded({ extended: true }));
 
+const User = mongoose.model('User', {
+  name: String,
+  dogs: Object
+});
+
+
+// INDEX
 app.get('/', (req, res) => {
-  res.render('home');
+  User.find()
+    .then(users => {
+    if (users.length == 0) {
+        res.render('new-user-form');
+        } else {
+         res.render('users-index', { users: users });
+ }
+    })
+});
+
+// NEW
+app.get('/users/new', (req, res) => {
+  res.render('new-user-form', {});
 })
 
-app.get('/users', (req, res) => {
-  res.render('users-index', { users: users });
+// CREATE
+app.post('/users', (req, res) => {
+  User.create(req.body).then((user) => {
+    res.redirect('/');
+  }).catch((err) => {
+    console.log(err.message);
+  })
 })
 
 app.listen(7000, () => {
