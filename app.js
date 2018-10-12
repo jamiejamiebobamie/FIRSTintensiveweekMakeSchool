69bb27e51b06541e36ac9b55ea596fe740be1823
jamiejamiebobamie <jmccrory@vt.edu>
2018-10-12 16:26:12 -0700

    photos

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const methodOverride = require('method-override')
 const app = express()
 
 var exphbs = require('express-handlebars');
@@ -14,39 +15,99 @@ app.set('view engine', 'handlebars');
 
 // The following line must appear AFTER const app = express() and before your routes!
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(methodOverride('_method'))
 
 const User = mongoose.model('User', {
   name: String,
-  dogs: Object
+  dog: Object,
+  street_address: String,
+  zip: String
+});
+
+const Dog = mongoose.model('Dog', {
+  name: String,
+  breed: String,
+  sex: String,
+  color: String,
+  notes: String
 });
 
 
-// INDEX
+//// INDEX
+//app.get('/', (req, res) => {
+//  User.find()
+//    .then(users => {
+//    if (users.length == 0) {
+//        res.render('new-user-form');
+//        } else {
+//         res.render('users-index', { users: users });
+// }
+//    })
+//});
+//
+//// NEW
+//app.get('/users/new', (req, res) => {
+//  res.render('new-user-form', {});
+//})
+//
+//// CREATE
+//app.post('/users', (req, res) => {
+//  User.create(req.body).then((user) => {
+//    res.redirect(`/users/${user._id}/edit`)
+//  }).catch((err) => {
+//    console.log(err.message);
+//  })
+//})
+//
+//// SHOW
+//app.get('/users/:id', (req, res) => {
+//  User.findById(req.params.id).then((user) => {
+//    res.render('users-show', { user: user })
+//  }).catch((err) => {
+//    console.log(err.message);
+//  })
+//})
+//
+//// EDIT
+//app.get('/users/:id/edit', (req, res) => {
+//  User.findById(req.params.id, function(err, user) {
+//    res.render('users-edit', {user: user});
+//  })
+//})
+//
+////Update
+//app.put('/users/:id', (req, res) => {
+//  User.findByIdAndUpdate(req.params.id, req.body)
+//    .then(user => {
+//      res.redirect(`/users/${user._id}`)
+//    })
+//    .catch(err => {
+//      console.log(err.message)
+//    })
+//})
+//
+//// NEW
+//app.get('/users/dogs/new', (req, res) => {
+//  res.render('new-dog-form', {});
+//})
+//
+//// CREATE
+//app.post('/users', (req, res) => {
+//  User.create(req.body).then((user) => {
+//    res.redirect(`/users/${user._id}/edit`)
+//  }).catch((err) => {
+//    console.log(err.message);
+//  })
+//})
+
+
+//Wireframe-Planning
+
+// Loading
 app.get('/', (req, res) => {
-  User.find()
-    .then(users => {
-    if (users.length == 0) {
-        res.render('new-user-form');
-        } else {
-         res.render('users-index', { users: users });
- }
-    })
+  res.render('loading', {});
 });
-
-// NEW
-app.get('/users/new', (req, res) => {
-  res.render('new-user-form', {});
-})
-
-// CREATE
-app.post('/users', (req, res) => {
-  User.create(req.body).then((user) => {
-    res.redirect('/');
-  }).catch((err) => {
-    console.log(err.message);
-  })
-})
 
 app.listen(7000, () => {
   console.log('App listening on port 7000!')
-})
+});
