a77cf239d1973012b717ed99ba9dbd8d837ab48a
jamiejamiebobamie <jmccrory@vt.edu>
2018-10-13 13:00:51 -0700

    added partials that I am not going to use

--- app.js
+++ app.js
@@ -33,80 +33,81 @@ const Dog = mongoose.model('Dog', {
 });
 
 
-//// INDEX
-//app.get('/', (req, res) => {
-//  User.find()
-//    .then(users => {
-//    if (users.length == 0) {
-//        res.render('new-user-form');
-//        } else {
-//         res.render('users-index', { users: users });
-// }
-//    })
-//});
-//
-//// NEW
-//app.get('/users/new', (req, res) => {
-//  res.render('new-user-form', {});
-//})
-//
-//// CREATE
-//app.post('/users', (req, res) => {
-//  User.create(req.body).then((user) => {
-//    res.redirect(`/users/${user._id}/edit`)
-//  }).catch((err) => {
-//    console.log(err.message);
-//  })
-//})
-//
-//// SHOW
-//app.get('/users/:id', (req, res) => {
-//  User.findById(req.params.id).then((user) => {
-//    res.render('users-show', { user: user })
-//  }).catch((err) => {
-//    console.log(err.message);
-//  })
-//})
-//
-//// EDIT
-//app.get('/users/:id/edit', (req, res) => {
-//  User.findById(req.params.id, function(err, user) {
-//    res.render('users-edit', {user: user});
-//  })
-//})
-//
-////Update
-//app.put('/users/:id', (req, res) => {
-//  User.findByIdAndUpdate(req.params.id, req.body)
-//    .then(user => {
-//      res.redirect(`/users/${user._id}`)
-//    })
-//    .catch(err => {
-//      console.log(err.message)
-//    })
-//})
-//
-//// NEW
-//app.get('/users/dogs/new', (req, res) => {
-//  res.render('new-dog-form', {});
-//})
-//
-//// CREATE
-//app.post('/users', (req, res) => {
-//  User.create(req.body).then((user) => {
-//    res.redirect(`/users/${user._id}/edit`)
-//  }).catch((err) => {
-//    console.log(err.message);
-//  })
-//})
+// see all users
+app.get('/', (req, res) => {
+  User.find()
+    .then(users => {
+    if (users.length == 0) {
+        res.render('new-user-form');
+        } else {
+         res.render('users-index', { users: users });
+ }
+    })
+});
+
+// get the NEW user form
+app.get('/users/new', (req, res) => {
+  res.render('new-user-form', {});
+})
+
+// CREATE a new user
+app.post('/users', (req, res) => {
+  User.create(req.body).then((user) => {
+    res.redirect(`/users/${user._id}/edit`)
+  }).catch((err) => {
+    console.log(err.message);
+  })
+})
+
+// view a user profile
+app.get('/users/:id', (req, res) => {
+  User.findById(req.params.id).then((user) => {
+    res.render('users-show', { user: user })
+  }).catch((err) => {
+    console.log(err.message);
+  })
+})
+
+// address info form (EDIT)
+app.get('/users/:id/edit', (req, res) => {
+  User.findById(req.params.id, function(err, user) {
+    res.render('users-edit', {user: user});
+  })
+})
+
+//add address info to user (update)
+app.put('/users/:id', (req, res) => {
+  User.findByIdAndUpdate(req.params.id, req.body)
+    .then(user => {
+      res.redirect(`/users/${user._id}/dogs/new`)
+    })
+    .catch(err => {
+      console.log(err.message)
+    })
+})
+
+// NEW dog
+app.get('/users/:id/dogs/new', (req, res) => {
+  res.render('new-dog-form', {});
+})
+
+// CREATE dog
+//add address info to user (update)
+app.post('/users/:id', (req, res) => {
+  Dog.create(req.body).then((dog) => {
+    res.redirect(`/users/${user._id}`)
+  }).catch((err) => {
+    console.log(err.message);
+  })
+})
 
 
 //Wireframe-Planning
 
 // Loading
-app.get('/', (req, res) => {
-  res.render('loading', {});
-});
+//app.get('/', (req, res) => {
+//  res.render('loading', {});
+//});
 
 app.listen(7000, () => {
   console.log('App listening on port 7000!')
